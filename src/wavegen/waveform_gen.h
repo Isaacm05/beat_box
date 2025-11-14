2bdeb0b9a030ec44491e4905e4e82814f06f549c
Vedant Sharma <Vedant2005.sharma@gmail.com>
2025-11-14 15:08:40 -0500

    named WaveParams Struct w Pots

--- src/wavegen/waveform_gen.h
+++ src/wavegen/waveform_gen.h
@@ -4,15 +4,16 @@
 #include <stdint.h>
 
 typedef struct {
-    float frequency;
-    float amplitude;
-    float decay;
-    int waveform_id; // 0=sine, 1=square, 2=triangle, 3=saw, 4=noise
-    float offset_dc;
-    float pitch_decay;
-    float noise_mix;
-    float env_curve;
-    float comp_amount;
+    float frequency;    // Pot 0
+    float amplitude;    // Pot 1
+    float decay;        // Pot 2
+    int waveform_id;    // 0=sine, 1=square, 2=triangle, 3=saw, 4=noise
+    float offset_dc;    // Pot 3
+
+    float pitch_decay;  // Pot 4
+    float noise_mix;    // Pot 5
+    float env_curve;    // Pot 6
+    float comp_amount;  // Pot 7
 } WaveParams;
 
 int waveform_generate(float* buffer, int max_samples, WaveParams* p);
